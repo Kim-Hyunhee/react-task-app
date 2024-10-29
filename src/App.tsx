26004735cb7b364cb37ba7df655829ab1381415e
Kim-Hyunhee <tta_bi@naver.com>
2024-10-29 14:31:19 +0900

    edit modal 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
-import { appContainer, board, buttons } from "./App.css";
-import BoardList from "./components/BoardList/BoardList";
-import ListsContainer from "./components/ListsContainer/ListsContainer";
-import { useTypedSelector } from "./hooks/redux";
+import { useState } from 'react';
+import { appContainer, board, buttons } from './App.css';
+import BoardList from './components/BoardList/BoardList';
+import ListsContainer from './components/ListsContainer/ListsContainer';
+import { useTypedSelector } from './hooks/redux';
+import EditModal from './components/EditModal/EditModal';
 
 function App() {
-  const [activeBoardId, setActiveBoardId] = useState("board-0");
-
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
   const boards = useTypedSelector((state) => state.boards.boardArray);
+
   const getActiveBoard = boards.filter(
     (board) => board.boardId === activeBoardId
   )[0];
+
   const lists = getActiveBoard.lists;
 
   return (
     <div className={appContainer}>
+      {modalActive ? <EditModal /> : null}
       <BoardList
         activeBoardId={activeBoardId}
         setActiveBoardId={setActiveBoardId}
